bfe39d523548ce94836ee4935cc6a69db97e2bfd
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-09-14 15:47:15 -0300

    fix: 🐛 fix

--- src/insights.py
+++ src/insights.py
@@ -37,9 +37,6 @@ def get_unique_industries(path):
     return industries
 
 
-if __name__ == '__main__':
-    get_unique_industries("./jobs.csv")
-
 def filter_by_industry(jobs, industry):
     """Filters a list of jobs by industry
 
